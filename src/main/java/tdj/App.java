6ab57b270ce050c858f5bf2fa53163097eb8aeaa
yito88 <yuji@phact-columba.com>
2020-09-10 18:44:27 +0900

    some tests

---
+++ src/main/java/tdj/App.java
@@ -0,0 +1,230 @@
+package tdj;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+public class App {
+  static final String HOST = "http://localhost:8000";
+  static final Region REGION = Region.AP_NORTHEAST_1;
+  static final int POPULATION_CONCURRENCY = 1;
+  static final int NUM_PER_THREAD = 1000;
+  static final int CONCURRENCY = 8;
+  static final String TABLE = "mytest";
+  static final String PARTITION_KEY = "pk";
+  static final String SORT_KEY = "sk";
+  static final String VALUE1 = "val1";
+  static final String VALUE2 = "val2";
+  static final String VALUE3 = "val3";
+  static final String VALUE4 = "val4";
+  static final String TEXT = "mytext";
+  static final AtomicInteger count = new AtomicInteger(0);
+
+  public static void main(String[] args) {
+    Storage storage = new Storage(HOST, REGION);
+
+    //storage.createTable(TABLE, PARTITION_KEY, SORT_KEY, new Long(100));
+    //try {
+    //  Thread.sleep(10000);
+    //} catch (InterruptedException e) {
+    //  Thread.currentThread().interrupt();
+    //}
+
+    // crudTest(storage);
+
+    conditionalInsertionsTest(storage);
+
+    // conditionalUpdatesTest(storage);
+
+    // populateItems(storage);
+
+    // storage.deleteTable(TABLE);
+
+    storage.close();
+  }
+
+  private static void crudTest(Storage storage) {
+    storage.put(TABLE, makeInitialValues(0));
+
+    Map<String, AttributeValue> key = makeKeyValues(0);
+    if (storage.get(TABLE, key) != makeInitialValues(0)) {
+      System.err.println("Unexpected result!");
+    }
+
+    Map<String, AttributeValue> updateValues = new HashMap<>();
+    updateValues.put(VALUE1, AttributeValue.builder().n(String.valueOf(1111)).build());
+    updateValues.put(VALUE3, AttributeValue.builder().bool(false).build());
+    storage.update(TABLE, key, updateValues);
+
+    Map<String, AttributeValue> newValues = makeInitialValues(0);
+    newValues.put(VALUE1, AttributeValue.builder().n(String.valueOf(1111)).build());
+    newValues.put(VALUE3, AttributeValue.builder().bool(false).build());
+    if (storage.get(TABLE, key) != newValues) {
+      System.err.println("Unexpected result!");
+    }
+
+    storage.delete(TABLE, key);
+
+    Map<String, AttributeValue> emptyValues = new HashMap<>();
+    if (storage.get(TABLE, key) != emptyValues) {
+      System.err.println("Unexpected result!");
+    }
+  }
+
+  private static Map<String, AttributeValue> makeKeyValues(int keyId) {
+    Map<String, AttributeValue> values = new HashMap<>();
+
+    String pk = String.valueOf(keyId);
+    values.put(PARTITION_KEY, AttributeValue.builder().s(pk).build());
+    String sk = String.valueOf(keyId % NUM_PER_THREAD);
+    values.put(SORT_KEY, AttributeValue.builder().n(sk).build());
+
+    return values;
+  }
+
+  private static Map<String, AttributeValue> makeInitialValues(int keyId) {
+    Map<String, AttributeValue> values = makeKeyValues(keyId);
+
+    String val1 = String.valueOf(0);
+    values.put(VALUE1, AttributeValue.builder().n(val1).build());
+
+    String val2 = TEXT + String.valueOf(keyId);
+    values.put(VALUE2, AttributeValue.builder().s(val2).build());
+
+    boolean val3 = (keyId % 3) == 0;
+    values.put(VALUE3, AttributeValue.builder().bool(val3).build());
+
+    byte[] val4 = (TEXT + String.valueOf(keyId)).getBytes(StandardCharsets.UTF_8);
+    values.put(VALUE4, AttributeValue.builder().b(SdkBytes.fromByteArray(val4)).build());
+
+    return values;
+  }
+
+  private static void conditionalInsertionsTest(Storage storage) {
+    ExecutorService es = Executors.newCachedThreadPool();
+    List<CompletableFuture> futures = new ArrayList<>();
+    IntStream.range(0, CONCURRENCY)
+        .forEach(
+            i -> {
+              CompletableFuture<Void> future =
+                  CompletableFuture.runAsync(
+                      () -> {
+                        new InsertionRunner(i, storage).run();
+                      },
+                      es);
+              futures.add(future);
+            });
+
+    CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
+
+    if (count.get() == NUM_PER_THREAD) {
+      System.out.println("The insertion test worked successfully!");
+    } else {
+      System.err.println("Unexpected result!");
+    }
+  }
+
+  private static class InsertionRunner {
+    private final int threadId;
+    private final Storage storage;
+
+    public InsertionRunner(int threadId, Storage storage) {
+      this.threadId = threadId;
+      this.storage = storage;
+    }
+
+    public void run() {
+      // multiple threads try to insert items with the same key
+      for (int i = 0; i < NUM_PER_THREAD; i++) {
+        try {
+          String conditions = "NOT " + PARTITION_KEY + " = :pk" + " AND NOT " + SORT_KEY + " = :sk";
+
+          // binding
+          Map<String, AttributeValue> expressionAttributeValues = new HashMap<>();
+          String pk = String.valueOf(i);
+          String sk = String.valueOf(i % NUM_PER_THREAD);
+          expressionAttributeValues.put(":pk", AttributeValue.builder().n(pk).build());
+          expressionAttributeValues.put(":sk", AttributeValue.builder().n(sk).build());
+
+          storage.putWithCondition(
+              TABLE, makeInitialValues(i), conditions, expressionAttributeValues);
+        } catch (Exception e) {
+          System.err.println(e.getMessage());
+          continue;
+        }
+        count.getAndIncrement();
+      }
+    }
+  }
+
+  private static void conditionalUpdatesTest(Storage storage) {
+    storage.put(TABLE, makeInitialValues(0));
+
+    ExecutorService es = Executors.newCachedThreadPool();
+    List<CompletableFuture> futures = new ArrayList<>();
+    IntStream.range(0, CONCURRENCY)
+        .forEach(
+            i -> {
+              CompletableFuture<Void> future =
+                  CompletableFuture.runAsync(
+                      () -> {
+                        new UpdateRunner(i, storage).run();
+                      },
+                      es);
+              futures.add(future);
+            });
+
+    CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
+
+    Map<String, AttributeValue> key = makeKeyValues(0);
+    Map<String, AttributeValue> result = storage.get(TABLE, key);
+    if (count.get() == 100 && Integer.valueOf(result.get(VALUE1).n()) == 100) {
+      System.out.println("The insertion test worked successfully!");
+    } else {
+      System.err.println("Unexpected result!");
+    }
+  }
+
+  private static class UpdateRunner {
+    private final int threadId;
+    private final Storage storage;
+
+    public UpdateRunner(int threadId, Storage storage) {
+      this.threadId = threadId;
+      this.storage = storage;
+    }
+
+    public void run() {
+      // multiple threads try to insert items with the same key
+      for (int i = 0; i < 100; i++) {
+        try {
+          String conditions = VALUE1 + " = :c1";
+          // binding
+          Map<String, AttributeValue> expressionAttributeValues = new HashMap<>();
+          String val1 = String.valueOf(i);
+          expressionAttributeValues.put(":c1", AttributeValue.builder().n(val1).build());
+
+          Map<String, AttributeValue> updateValues = new HashMap<>();
+          updateValues.put(VALUE1, AttributeValue.builder().n(String.valueOf(i + 1)).build());
+
+          storage.updateWithCondition(
+              TABLE, makeKeyValues(0), updateValues, conditions, expressionAttributeValues);
+        } catch (Exception e) {
+          System.err.println(e.getMessage());
+          continue;
+        }
+        count.getAndIncrement();
+      }
+    }
+  }
+}
---
+++ src/main/java/tdj/Storage.java
@@ -0,0 +1,187 @@
+package tdj;
+
+import java.util.HashMap;
+import java.util.Map;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeAction;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.DeleteTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+
+public class Storage {
+  private final DynamoDbClient client;
+
+  public Storage(String host, Region region) {
+    this.client = DynamoDbClient.builder().region(region).build();
+  }
+
+  public void close() {
+    client.close();
+  }
+
+  public void createTable(String table, String partitionKey, Long throughput)
+      throws DynamoDbException {
+    // TODO: change the type
+    CreateTableRequest request =
+        CreateTableRequest.builder()
+            .attributeDefinitions(
+                AttributeDefinition.builder()
+                    .attributeName(partitionKey)
+                    .attributeType(ScalarAttributeType.S)
+                    .build())
+            .keySchema(
+                KeySchemaElement.builder()
+                    .attributeName(partitionKey)
+                    .keyType(KeyType.HASH)
+                    .build())
+            .provisionedThroughput(
+                ProvisionedThroughput.builder()
+                    .readCapacityUnits(throughput)
+                    .writeCapacityUnits(throughput)
+                    .build())
+            .tableName(table)
+            .build();
+
+    client.createTable(request);
+  }
+
+  public void createTable(String table, String partitionKey, String sortKey, Long throughput)
+      throws DynamoDbException {
+    // TODO: change the type
+    CreateTableRequest request =
+        CreateTableRequest.builder()
+            .attributeDefinitions(
+                AttributeDefinition.builder()
+                    .attributeName(partitionKey)
+                    .attributeType(ScalarAttributeType.S)
+                    .build(),
+                AttributeDefinition.builder()
+                    .attributeName(sortKey)
+                    .attributeType(ScalarAttributeType.N)
+                    .build())
+            .keySchema(
+                KeySchemaElement.builder()
+                    .attributeName(partitionKey)
+                    .keyType(KeyType.HASH)
+                    .build(),
+                KeySchemaElement.builder().attributeName(sortKey).keyType(KeyType.RANGE).build())
+            .provisionedThroughput(
+                ProvisionedThroughput.builder()
+                    .readCapacityUnits(throughput)
+                    .writeCapacityUnits(throughput)
+                    .build())
+            .tableName(table)
+            .build();
+
+    client.createTable(request);
+  }
+
+  public void deleteTable(String table) throws DynamoDbException {
+    DeleteTableRequest request = DeleteTableRequest.builder().tableName(table).build();
+
+    client.deleteTable(request);
+  }
+
+  public void put(String table, Map<String, AttributeValue> values) throws DynamoDbException {
+    PutItemRequest request = PutItemRequest.builder().tableName(table).item(values).build();
+
+    client.putItem(request);
+  }
+
+  public void putWithCondition(
+      String table,
+      Map<String, AttributeValue> values,
+      String conditions,
+      Map<String, AttributeValue> expressionAttributeValues)
+      throws DynamoDbException {
+    PutItemRequest request =
+        PutItemRequest.builder()
+            .tableName(table)
+            .item(values)
+            .conditionExpression(conditions)
+            .expressionAttributeValues(expressionAttributeValues)
+            .build();
+
+    client.putItem(request);
+  }
+
+  public void update(
+      String table, Map<String, AttributeValue> key, Map<String, AttributeValue> values)
+      throws DynamoDbException {
+    Map<String, AttributeValueUpdate> updateValues = new HashMap<>();
+    values.forEach(
+        (a, v) -> {
+          updateValues.put(
+              a, AttributeValueUpdate.builder().value(v).action(AttributeAction.PUT).build());
+        });
+
+    UpdateItemRequest request =
+        UpdateItemRequest.builder()
+            .tableName(table)
+            .key(key)
+            .attributeUpdates(updateValues)
+            .build();
+
+    client.updateItem(request);
+  }
+
+  public void updateWithCondition(
+      String table,
+      Map<String, AttributeValue> key,
+      Map<String, AttributeValue> values,
+      String conditions,
+      Map<String, AttributeValue> expressionAttributeValues)
+      throws DynamoDbException {
+    String updateExpression = "SET ";
+    int i = 0;
+    for (Map.Entry<String, AttributeValue> entry : values.entrySet()) {
+      String a = entry.getKey();
+      AttributeValue v = entry.getValue();
+
+      updateExpression += a + "=:v" + String.valueOf(i);
+      expressionAttributeValues.put(":v" + String.valueOf(i), v);
+
+      i++;
+      if (i < values.size()) {
+        updateExpression += ",";
+      }
+    }
+
+    UpdateItemRequest request =
+        UpdateItemRequest.builder()
+            .tableName(table)
+            .key(key)
+            .updateExpression(updateExpression)
+            .conditionExpression(conditions)
+            .expressionAttributeValues(expressionAttributeValues)
+            .build();
+
+    client.updateItem(request);
+  }
+
+  public Map<String, AttributeValue> get(String table, Map<String, AttributeValue> key)
+      throws DynamoDbException {
+    GetItemRequest request =
+        GetItemRequest.builder().tableName(table).key(key).consistentRead(true).build();
+
+    return client.getItem(request).item();
+  }
+
+  public void delete(String table, Map<String, AttributeValue> key) throws DynamoDbException {
+    DeleteItemRequest request = DeleteItemRequest.builder().tableName(table).key(key).build();
+
+    client.deleteItem(request);
+  }
+}
